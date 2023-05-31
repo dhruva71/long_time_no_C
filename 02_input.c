2e24d69a635b9e10edcd4ce8787c0b075576a21a
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2023-05-31 18:51:31 +0530

    Added 02_input.c

--- 02_input.c
+++ 02_input.c
@@ -6,7 +6,12 @@ int main() {
     int a;
     // initialization of variable a
     // always initialize your variables before using them to avoid garbage values
-    a = 10
+    printf("Enter a value for a: ");
+
+    // scanf is a function that comes from stdio.h
+    // %d is a format specifier for integers
+    // &a means that the value provided will be stored in the memory address of variable a
+    scanf("%d", &a);
 
     // declaration and initialization of variable b of type int in the same line
     int b = 20;
@@ -17,7 +22,7 @@ int main() {
     // display the value of variable c
     // %d is a format specifier for integers
     // printf is a function that comes from stdio.h
-    printf("The sum of a and b is %d\n", c);
+    printf("a+20 = %d\n", c);
 
     // return 0 means that the program ended successfully
     return 0;
